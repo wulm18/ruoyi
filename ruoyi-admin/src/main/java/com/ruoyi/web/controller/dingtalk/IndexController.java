600fade454e399bb7ac546c8b9fb1dcf920180e3
mtcnn <876734111@qq.com>
2020-06-01 15:57:04 +0800

    添加任务清单

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/dingtalk/IndexController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/dingtalk/IndexController.java
@@ -1,17 +1,20 @@
 package com.ruoyi.web.controller.dingtalk;
 
-import com.ruoyi.dingtalk.config.URLConstant;
 import com.dingtalk.api.DefaultDingTalkClient;
 import com.dingtalk.api.DingTalkClient;
 import com.dingtalk.api.request.OapiUserGetRequest;
 import com.dingtalk.api.request.OapiUserGetuserinfoRequest;
 import com.dingtalk.api.response.OapiUserGetResponse;
 import com.dingtalk.api.response.OapiUserGetuserinfoResponse;
-import com.taobao.api.ApiException;
+import com.ruoyi.dingtalk.config.URLConstant;
 import com.ruoyi.dingtalk.util.AccessTokenUtil;
 import com.ruoyi.dingtalk.util.ServiceResult;
+import com.ruoyi.system.domain.SysUser;
+import com.ruoyi.system.service.ISysUserService;
+import com.taobao.api.ApiException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -24,6 +27,8 @@ import java.util.Map;
 @RequestMapping("/dingtalk")
 public class IndexController {
     private static final Logger bizLogger = LoggerFactory.getLogger(IndexController.class);
+    @Autowired
+    private ISysUserService userService;
 
     /**
      * 欢迎页面,通过url访问，判断后端服务是否启动
@@ -40,7 +45,7 @@ public class IndexController {
      */
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
-    public ServiceResult login(@RequestParam(value = "authCode") String requestAuthCode) {
+    public ServiceResult login(@RequestParam(value = "authCode") String requestAuthCode) throws ApiException{
         //获取accessToken,注意正是代码要有异常流处理
         String accessToken = AccessTokenUtil.getToken();
 
@@ -63,10 +68,18 @@ public class IndexController {
 
         String userName = getUserName(accessToken, userId);
         System.out.println(userName);
+        String userMobile = getUserMobile(accessToken, userId);
+        System.out.println(userMobile);
+
+        //获取党建助手系统中用户信息
+        SysUser sysUser = userService.selectUserByPhoneNumber(userMobile);
+        System.out.println(userMobile);
         //返回结果
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("userId", userId);
         resultMap.put("userName", userName);
+        resultMap.put("userMobile", userMobile);
+        resultMap.put("sysUser", sysUser);
         ServiceResult serviceResult = ServiceResult.success(resultMap);
         return serviceResult;
     }
@@ -86,6 +99,27 @@ public class IndexController {
             request.setHttpMethod("GET");
             OapiUserGetResponse response = client.execute(request, accessToken);
             return response.getName();
+        } catch (ApiException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 获取用户手机号
+     *
+     * @param accessToken
+     * @param userId
+     * @return
+     */
+    private String getUserMobile(String accessToken, String userId) {
+        try {
+            DingTalkClient client = new DefaultDingTalkClient(URLConstant.URL_USER_GET);
+            OapiUserGetRequest request = new OapiUserGetRequest();
+            request.setUserid(userId);
+            request.setHttpMethod("GET");
+            OapiUserGetResponse response = client.execute(request, accessToken);
+            return response.getMobile();
         } catch (ApiException e) {
             e.printStackTrace();
             return null;
